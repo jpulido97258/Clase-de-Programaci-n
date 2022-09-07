aeb825f0ee05b65dc40acf7c341bf4bfcb9368e9
DasherPR <camiloandres1503@gmail.com>
2022-09-07 12:41:33 -0500

    Añadi el ejercicio del bucle for

---
+++ TalleresPython/Clase3/EjercicioBucleFor.py
@@ -0,0 +1,7 @@
+# Este es un bucle for
+
+n = 15
+
+for i in range (n): #posicion inicial
+    print("El resultado va a ser la posicion final", i);
+print("El resultado final es el producto del conteo");
