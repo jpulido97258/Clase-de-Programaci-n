4b12c5561be753b1812691fa50fb2721c2a0ce9a
DasherPR <camiloandres1503@gmail.com>
2022-09-07 12:27:25 -0500

    añadi otro ejercicio

---
+++ TalleresPython/Clase3/EjercicioMulti2.py
@@ -0,0 +1,18 @@
+#Segundo ejercios multivariables
+
+num1 = int(input("escribe el primer numero: "));
+num2 = int(input("escribe el segundo numero: "));
+
+
+resultado1 = num1%2;
+resultado2 = num2%3;
+print("El resultado inicial es:" , resultado1);
+print("El resultado secundario es:" , resultado2);
+
+#booleano
+condicion1 = resultado1 != resultado2;
+
+print("El resultado es: " , condicion1, "Si el resultado es true vuelve a escribir los numeros. ");
+
+
+
