f9a0758fa6eda3b75a0fbb1159443e85b1c9e400
Tonalhydr469 <112195097+Tonalhydr469@users.noreply.github.com>
2022-09-07 12:56:01 -0500

    Add files via upload

---
+++ Ejercicio 3.py
@@ -0,0 +1,19 @@
+#Variable multi funcion
+
+
+import math
+
+peso= float (input("INGRESE SU PESO CORPORAL"))#ingrese un valor numerico en kilogramos
+estatura= float (input("INGRESE SU ESTATURA EN METROS"))#ingrese un valor numerico
+
+IMC = round (peso/math.pow(estatura,2),1)#formula para sacar la potencia de el peso y la masa
+
+print ("su IMC es de"+str(IMC))#me muestra el resultado en cadena de texto IMC
+
+lista = [("composicion corporal","indice de masa corporal(IMC")("pesoinferior al no")]
+def = inc(masa/ estatura)
+
+masa = int(30)
+estatura = float(1.75)
+
+print (def)
