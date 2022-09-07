ef6ca9fbe5a0df43406623d94de8af12de166b71
Tonalhydr469 <112195097+Tonalhydr469@users.noreply.github.com>
2022-09-07 12:45:00 -0500

    Add files via upload

---
+++ Ejercicio 1.py
@@ -0,0 +1,6 @@
+#Escriba su nombre,apellido y edad
+
+print ("escriba su nombre")
+input= (nombre)
+print("escriba su apellido")
+input= (apellido)
---
+++ Teorema_de_Tales.py
@@ -0,0 +1,10 @@
+#Teorema de Tales
+
+b=float(input("Inserte el valor del lado BD:"))
+c=float(input("Inserte el valor del lado AC:"))
+d=float(input("Inserte el valor del lado CE:"))
+print ("El lado BD es:", b)
+print ("El lado BD es:", c)
+print ("El lado BD es:", d)
+a=((c*b)/(d))
+print("El valor del lado AB es de:", a)
