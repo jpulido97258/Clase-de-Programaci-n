67fd3285241457dc52f24795e63dd70bd73bdb67
Julian Alfonso <112194849+Juli3845@users.noreply.github.com>
2022-09-09 12:02:28 -0500

    Ejercicio Usuario/contraseña

---
+++ Julian-Talleres-PSeint/Ejercicio1.py
@@ -0,0 +1,11 @@
+usuario = "admin"
+contra = "596"
+print("Ingrese su usuario y contraseña")
+print("Usuario:")
+usuario1 = input()
+print("Contraseña:")
+contra1 = input()
+if usuario == usuario1 and contra == contra1:
+    print("Bienvenido!!")
+else:
+    print("Usuario o contraseña incorrecto")
