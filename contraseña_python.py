ddb548cfad371912ce9d58af58becd25aa30c561
Jorge Andrés Mellizo Celis <112194881+JorgeMellizo@users.noreply.github.com>
2022-09-09 12:56:07 -0500

    Add files via upload

---
+++ contraseña_python.py
@@ -0,0 +1,30 @@
+#Ejercicio de contraseña 
+
+
+
+
+print("Escriba su usuario")
+a=str(input("Su usuario es: "))
+
+if a ==("Jorge"):
+    print("Su usuario es correcto")
+elif a ==("Daniel"):
+    print("Su usuario es correcto")
+else:
+    print("Su usuario es inconrrecto")
+
+print("Escriba su contraseña")
+password=float(input("Escriba su contraseña numerica"))
+
+if password ==(1234):
+    print("Su contraseña es correcta")
+else:
+    print("Su contraseña es inconrrecta")
+
+
+
+
+
+
+# while 
+
