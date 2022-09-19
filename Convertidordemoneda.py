63582721a938602b7abe04c1e3350ae2e27a9ce6
Jorge Andrés Mellizo Celis <112194881+JorgeMellizo@users.noreply.github.com>
2022-09-19 11:58:17 -0500

    Add files via upload

--- Convertidordemoneda.py
+++ Convertidordemoneda.py
@@ -5,26 +5,89 @@
 print("Bienvenidos al convertidor de moneda ")
 print("Que moneda quieres convertir")
 
-A= ("Dolar, Euro, Peso Colombiano, Peso Argentino, Yuan Chino, libra esterlina")
-#print(A)
-print(A)
-print("Seleccione la posicion, 0:5")
-B=input()
-moneda=str(A.index(B)) #Corregir error 
-print(moneda) #error  #Traceback (most recent call last):
-  #File "c:\Users\SALA\Documents\Algoritmos Jorge\Ejercicios\ejercicios_python\Python_Ej\ProyectoPython\Convertidordemoneda.py", line 13, in <module>
-    #moneda=str(A.index(B))
-#ValueError: substring not found
+monedas = ["DOLAR", "EURO", "PESO COLOMBIANO", "PESO ARGENTINO", "YUAN CHINO", "LIBRA ESTERLINA"]
 
-if moneda == "Dolar":
+criptomoneda=("BITCOIN, ETHERUM")
+print(criptomoneda)
+print(monedas)
+try:
+    moneda=monedas[(monedas.index(input().upper()))] #Corregir error 
+except:
+    print("el valor que pusiste no coincide con los aceptados")
+print(moneda)
+
+if moneda == "DOLAR":
     print("Su moneda es: ", moneda)
-    input("Que cantidad de dinero requieres convertir")
-elif moneda == "Euro":
+    monto1=int(input("Que cantidad de dinero que requieres convertir"))
+    monedanueva1= monedas[(monedas.index(input("¿A que moneda desea convertir?:")))]
+    if monedanueva1 == "EURO":
+        monedaconvertida= monto1*1
+        print(monto1, "Estos dolares equivalen a:", monedaconvertida, "Euros")
+elif moneda == "EURO":
     print("Su moneda es: ", moneda)
-    input("Digite la cantidad de dinero ")
+    input("Que la cantidad de dinero que requieres convertir")
+    monto2=int(input())
+    input("¿A que moneda desea convertir?:")
+    monedanueva2= input()
+    if monedanueva2   == "DOLAR":
+        monedaconvertida2= monto2*1
+        print(monto2, "Estos dolares equivalen a:", monedaconvertida2, "Euros")
+elif moneda == "PESO COLOMBIANO":
+    print("Su moneda es: ", moneda)
+    input("Que la cantidad de dinero que requieres convertir")
+    monto3=int(input())
+    input("¿A que moneda desea convertir?:")
+    monedanueva3= input()
+    if monedanueva3   == "EURO":
+        monedaconvertida3= monto3*1
+        print(monto3, "Estos dolares equivalen a:", monedaconvertida3, "Euros")
+elif moneda   == "PESO ARGENTINO":
+    print("Su moneda es: ", moneda)
+    input("Que la cantidad de dinero que requieres convertir")
+    monto4=int(input())
+    input("¿A que moneda desea convertir?:")
+    monedanueva4= input()
+    if monedanueva4   == "EURO":
+        monedaconvertida4= monto4*1
+        print(monto4, "Estos dolares equivalen a:", monedaconvertida4, "Euros")
+elif moneda   == "YUAN CHINO":
+    print("Su moneda es: ", moneda)
+    input("Que la cantidad de dinero que requieres convertir")
+    monto5=int(input())
+    input("¿A que moneda desea convertir?:")
+    monedanueva5= input()
+    if monedanueva5   == "EURO":
+        monedaconvertida5= monto5*1
+        print(monto5, "Estos dolares equivalen a:", monedaconvertida5, "Euros")
+elif moneda   == "LIBRA ESTERLINA":
+    print("Su moneda es: ", moneda)
+    input("Que la cantidad de dinero que requieres convertir")
+    monto6=int(input())
+    input("¿A que moneda desea convertir?:")
+    monedanueva6= input()
+    if monedanueva6   == "EURO":
+        monedaconvertida6= monto6*1
+        print(monto6, "Estos dolares equivalen a:", monedaconvertida6, "Euros")
+elif moneda   == "BITCOIN":
+    print("Su moneda es: ", moneda)
+    input("Que la cantidad de dinero que requieres convertir")
+    montoxxxxxx=int(input())
+    input("¿A que moneda desea convertir?:")
+    monedanuevaxxxxxx= input()
+    if monedanuevaxxxxxx   == "EURO":
+        monedaconvertidaxxxxx= montoxxxxxx*1
+        print(montoxxxxxx, "Estos dolares equivalen a:", monedaconvertidaxxxxx, "Euros")
+elif moneda   == "ETHERUM":
+    print("Su moneda es: ", moneda)
+    input("Que la cantidad de dinero que requieres convertir")
+    montozzz=int(input())
+    input("¿A que moneda desea convertir?:")
+    monedanuevazzz= input()
+    if monedanuevazzz   == "EURO":
+        monedaconvertidazzz= montozzz*1
+        print(montozzz, "Estos dolares equivalen a:", monedaconvertidazzz, "Euros")
 else: 
-    print("No existe esa moneda")
-
+    print("Digite una moneda existente en y habilitada en el conversor", monedas , criptomoneda, "Estas son las disponibles a conversion")
 
 
 
