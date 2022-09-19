6db08aecf7aadb302864ff6ad8a0a0e8fbbc552b
Edward Quintero Romero <112195004+Edwardqr20@users.noreply.github.com>
2022-09-19 11:51:26 -0500

    Add files via upload

---
+++ Proyecto Final/Proyecto_final.py
@@ -0,0 +1,22 @@
+#Codigo Trabajo Final
+#Este proyecto se llamara MIRENDI
+#Primero ingresar DAtos generales
+print('Bienvenido a nuestro sistema')
+print('Por favor ingresa tus datos para empezar')
+print('¿Eres Hombre o Mujer?')
+genero=input()
+print('Ingresa tu edad')
+edad= input()
+print('Digita en cm cuanto mides')
+altura=input()
+print('¿Cual es tu peso en kg? el verdadero xd' )
+peso=input()
+#Seleccionar que proceso quiere realizar
+print('¿Que quieres realizar el dia de hoy:')
+print('Digita 1 para realizar una evaluacion general')
+print('Digita 2 para realizar una evaluacion especifica')
+proceso=input()
+if proceso==1:
+
+if proceso==2:
+
